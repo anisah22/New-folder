72a896beb3c71cb277cef4021529e767a1cb83b4
anisah22 <anisahnailatul@gmail.com>
2024-07-03 11:11:27 +0700

    nambah volume balok

--- main.c
+++ main.c
@@ -5,6 +5,12 @@ float hitungLuasSegitiga(int alas, int tinggi){
     return luas; 
 }
 
+int volume_balok (int panjang, int lebar, int tinggi){
+    int hasil;
+    hasil = panjang * lebar * tinggi;
+    return hasil;
+}
+
 int main(){
     printf("halo\n");
 
@@ -16,7 +22,10 @@ int main(){
     printf("nilai integer adalah %d, berlokasi di pointer memori %p\n", a, pa);
 
     float luasan = hitungLuasSegitiga(a,b);
-    printf("nilai luasan : %0.2f", luasan);
+    printf("nilai luasan : %0.2f\n", luasan);
+
+    int hasil_balok = volume_balok (4, 5, 10);
+    printf("volume_balok : %d", hasil_balok); 
 
     return 0;
 }
