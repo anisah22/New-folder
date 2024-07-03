50a951bf91aa66f55745e15fec934935b6998a40
anisah22 <anisahnailatul@gmail.com>
2024-07-03 11:27:05 +0700

    nambah volume balok

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 def hitungsegitga(alas, tinggi):
     return(0.5*alas*tinggi)
 
+def volume_balok(panjang, lebar, tinggi):
+    hasil = panjang * lebar * tinggi
+    return hasil
 
 a = 3
 b = 5
 
 luas = hitungsegitga(a,b)
+
+volume = volume_balok(4, 5, 10)
+print("volume balok :", volume)
 
 print("a =",a,"dan b",b,"dengan hasil",luas)
