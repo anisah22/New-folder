58d3c3eda5a4c41442330f8522931d66cd21eae4
anisah22 <anisahnailatul@gmail.com>
2024-07-03 19:53:31 +0700

    fungsi if

--- main.py
+++ main.py
@@ -14,3 +14,34 @@ volume = volume_balok(4, 5, 10)
 print("volume balok :", volume)
 
 print("a =",a,"dan b",b,"dengan hasil",luas)
+
+bebas = 'a'
+baru = "bola"
+sapi = bebas+baru
+print(sapi)
+
+baris = [1,2,3]
+print(baris[1])
+
+baris2 = [[2,4,3],
+          [1,5,7],
+          [0,9,5]]
+
+print(baris2[1][2])
+
+print(baru[2])
+
+i = 0
+iter = 1
+penentu = 1
+if (penentu==1):
+    while (iter==1) :
+        if (i==4):
+            iter = 0
+
+        print("hore",i+1,iter)
+        i+=1
+elif (penentu==0) :
+    print("baiyoh")
+else :
+    print("ok")
